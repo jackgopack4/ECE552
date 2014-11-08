2576af622105c53606e3dc1e50d73d46176e9f02
John L. Peterson <jackgopack4@gmail.com>
2014-11-08 02:36:27 -0600

    updated arithmod

--- ALU/arithmod.v
+++ ALU/arithmod.v
@@ -6,12 +6,13 @@ module arithmod(Out, V, N, Z, A, B, Sel);
   output V, N, Z;
 
   wire cout;
-  wire [15:0] adder_out, paddsb_out, adder_result, B_nor, sel_nor;
+  wire [15:0] adder_out, paddsb_out, adder_result, B_xor, sel_xor;
   wire sign;
-  split16 splitSel(sel_nor, Sel[1]);
-  assign B_nor = (sel_nor) ? (~B + 1) : B;
-  add16bit Adder(cout, adder_out, A, B_nor, Sel[1]);
-  overflow check(V, sign, A[15], B_nor[15], adder_out[15], Sel[1]);
+  split16 splitSel(sel_xor, Sel[1]);
+  assign B_nor = (sel_xor ^ B);
+  paddsb ps(paddsb_out, A, B);
+  add16bit Adder(cout, adder_out, A, B_xor, Sel[1]);
+  overflow check(V, sign, A[15], B_xor[15], adder_out[15], Sel[1]);
   assign adder_result = (V) ? ((sign) ? 16'h8000 : 16'h7FFF):
 	                       adder_out;
   assign Out = (Sel[0]) ? paddsb_out :
