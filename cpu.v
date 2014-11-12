0d4e5da8c3c9a787c9d45549915c9d7675b8b496
drhartman <drhartman9@gmail.com>
2014-11-12 07:18:52 -0600

    Update cpu.v

--- cpu.v
+++ cpu.v
@@ -12,11 +12,11 @@ output hlt;
 
 // PC //
 reg [15:0] nextAddr; 
-reg [15:0] programCounter;
+wire [15:0] programCounter, PCaddOut, PCNext;
 reg [15:0] pcInc;	// to mediate between PC and nextAddr
 
 // Instruction Memory //
-reg [15:0] instruction;
+wire reg [15:0] instruction;
 wire rd_en;			 // asserted when instruction read desired
 
 // Register Memory //
@@ -41,6 +41,9 @@ wire Yes;
 // Sign Extension
 wire [15:0] signOutALU, signOutJump, signOutBranch;
 
+// Flag Register
+wire zrOut, negOut, ovOut, change_z, change_v, change_n;
+
 // Data Memory //
 //wire re;	// asserted when instruction read desired
 //wire we;	// asserted when write desired
@@ -55,7 +58,7 @@ wire 	RegDst,		// 1: write back instr[11:8] to register, 0: (sw don't care) lw i
 		MemWrite,	// 1: enable memory write, 0: disable memory write
 		ALUSrc, 	// 0: readData2 -> ALU 1: sign-extended -> ALU
 		RegWrite,	// 1: enable right back to register, 0: don't write back to register
-		PCSrc,		// 0: take next pc addr, 1: enable branch (sign-ext << 2)
+		PCSrc,		// 0: take next pc addr, 1: enable branch (sign-ext)
 		// PCSrc is not connected to the control
 		LoadHigh,	// 1: take in [11:8] as read data 1, 0: take normal read data 1
 		Jump;
@@ -72,31 +75,46 @@ PC pcMod(.nextAddr(nextAddr), .clk(clk), .rst(rst_n), .programCounter(programCou
 always @(posedge clk) begin
 	if (~rst_n) begin
 	pcInc <= 16'h0000;
-	end
-	else if (PCSrc) begin
+	//end
+	//else if (PCSrc) begin
 	// not sure if this would work...
-	nextAddr <= pcInc + 1 + (signOutBranch << 2);
-	end else if (Jump) begin
-	nextAddr <= pcInc + 1 + (signOutJump << 2);
+	//pcInc <= (pcInc + 1 + signOutBranch); //<< 2);
+	//nextAddr <= pcInc;
+	//end else if (Jump) begin
+	//pcInc <= (pcInc + 1 + signOutJump); //<< 2);
+	//nextAddr <= pcInc;
 	end else begin
 	pcInc <= programCounter + 1;
-	nextAddr <= pcInc;
+	nextAddr <= PCNext;
 	end
 	// let's see what's going on!
-	$display("programCounter=%d, instruction=%b, readData1=%b, src2Wire=%b, ALUResult=%b, dst_addr=%b, dst=%b, ALUSrc=%b, Branch=%b, Yes=%b, PCSrc=%b, nextAddr=%b\n", programCounter, instruction, readData1, src2Wire, ALUResult, dst_addr, dst, ALUSrc, Branch, Yes, PCSrc, nextAddr);
+	$display("programCounter=%d, instruction=%b, readData1=%b,
+	src2Wire=%b, ALUResult=%b, dst_addr=%b, dst=%b, ALUSrc=%b, Branch=%b, Yes=%b, PCSrc=%b, nextAddr=%b,
+	negOut=%b, ovOut=%b, zrOut=%b, signOutBranch=%b, pcInc=%b\n", programCounter, instruction,
+	readData1, src2Wire, ALUResult, dst_addr, dst, ALUSrc, Branch, Yes, PCSrc, nextAddr,
+	negOut, ovOut, zrOut, signOutBranch, pcInc);
 	//$display("programCounter=%d, ALUResult=%b, dst_addr=%b, dst=%b \n", programCounter, ALUResult,
 	//dst_addr, dst);
 	
 end
 
+// PC Adder
+alu2 PCAdd(PCaddOut, pcInc, signOutBranch);
+assign PCNext = PCSrc ? PCaddOut : pcInc;
+
+
 // Sign-extender
 sign_extenderALU signExtenALU(instruction[7:0], signOutALU);
 sign_extenderJump signExtenJUMP(instruction[11:0], signOutJump);
-sign_extenderBranch signExtenBranch(instruction[3:0], signOutBranch);
+sign_extenderBranch signExtenBranch(instruction[8:0], signOutBranch);
 
 // Branch 
-branch_met BranchPred(.Yes(Yes), .ccc(instruction[11:9]), .N(neg), .V(ov), .Z(zr));
+branch_met BranchPred(.Yes(Yes), .ccc(instruction[11:9]), .N(negOut), .V(ovOut), .Z(zrOut));
 assign PCSrc = (Yes && Branch);
+
+// Flags
+flags flagReg(.zr(zrOut), .neg(negOut), .ov(ovOut), .change_z(change_z), .change_n(change_n), 
+.change_v(change_v), .z_in(zr), .n_in(neg), .v_in(ov));
 
 // Instruction Memory //
 IM instMem(.clk(clk), .addr(programCounter), .rd_en(rd_en), .instr(instruction));
@@ -119,7 +137,8 @@ assign readReg1 = LoadHigh ? instruction[11:8] : instruction[7:4];
 
 // ALU //
 ALU alu(.src0(readData1), .src1(src2Wire), .op(instruction[15:12]), 
-.dst(ALUResult), .ov(ov), .zr(zr), .neg(neg), .shamt(shamt));
+.dst(ALUResult), .ov(ov), .zr(zr), .neg(neg), .shamt(shamt), .change_v(change_v),
+.change_z(change_z), .change_n(change_n));
 // MUX: lw/sw instruction use the sign-extended value for src1 input
 assign src2Wire = ALUSrc ? signOutALU : readData2;
 
