60113d710748ec77865b0a6dff53dbbede006ecb
drhartman <drhartman9@gmail.com>
2014-12-08 06:14:07 -0600

    Update mem_hierarchy.v

--- mem_hierarchy.v
+++ mem_hierarchy.v
@@ -17,55 +17,64 @@ module mem_hierarchy(clk, rst_n, instr, i_rdy, d_rdy, rd_data, i_addr, d_addr, r
   wire m_re, m_we, m_rdy;
   wire [63:0] m_wr_data, m_rd_data;
   
-  cache iCache( clk,
+	cache iCache( clk,
                 rst_n,
                 i_addr[15:2],
-                i_wr_data,
-                1'b0,
-                i_we,
-                1'b1,
-                i_rd_data,
-                i_tag,
-                i_hit,
-                i_dirty );
-
-  unified_mem memory( clk,
-                      rst_n,
-                      m_addr,
-                      m_re,
-                      m_we,
-                      m_wr_data,
-                      m_rd_data,
-                      m_rdy);
-
-  cache_controller controller(.clk(clk),
+                i_wr_data, //m_rd_data,	
+                1'b0,		// dirty bit set to 0
+                i_we,		// enable write to cache
+                1'b1,		// read enable set high
+                i_rd_data,	// i_cache read out data is full block
+                i_tag,		// tag of instr
+                i_hit,		// high if hit
+                i_dirty );	// high if dirty
+	
+	cache_controller controller(.clk(clk),
                               .rst_n(rst_n),
-                              .i_rdy(i_rdy),
-                              .i_sel(i_sel),
-                              .i_wr_data(i_wr_data),
-                              .i_we(i_we),
-                              .m_addr(m_addr),
-                              .m_re(m_re),
-                              .m_we(m_we),
-                              .m_wr_data(m_wr_data),
-                              .i_addr(i_addr),
-                              .i_hit(i_hit),
-                              .i_tag(i_tag),
-                              .m_rd_data(m_rd_data),
-                              .m_rdy(m_rdy),
-                              .re(re),
-                              .we(we),
+                              .i_rdy(i_rdy),	// high when instr is ready to be read
+                              .i_sel(i_sel),	// sel for instr output
+                              .i_wr_data(i_wr_data),	// instr to write to cache
+                              .i_we(i_we),		// enable write to cache
+                              .m_addr(m_addr),	// mem addr (whether in cache or main mem)
+                              .m_re(m_re),		// mem read enable
+                              .m_we(m_we),		// mem write enable
+                              .m_wr_data(m_wr_data),	// mem to write to cache
+                              .i_addr(i_addr),	// taking full i_addr into conroller
+                              .i_hit(i_hit),	// instr hit
+                              .i_tag(i_tag),	// tag
+                              .m_rd_data(m_rd_data),	// data read from mem
+                              .m_rdy(m_rdy),	// mem ready? for hwat?
+                              .re(re),			// read enable? for hwat?
+                              .we(we),			// write enable?
                               .d_addr(d_addr),
                               .wrt_data(wrt_data));
+	
+	unified_mem memory( clk,
+                      rst_n,
+                      m_addr,	// addr in mem
+                      m_re,		// read enable
+                      m_we,		// write enable
+                      m_wr_data,	// data to write to mem
+                      m_rd_data,	// data read from mem
+                      m_rdy);		// mem has finished reading/writing
+
+
   
   // mux for instruction output
-  always@(*) begin
+  always@(negedge clk) begin
     if     (i_sel == 2'b00) instr = i_rd_data[15:0];
     else if(i_sel == 2'b01) instr = i_rd_data[31:16];
     else if(i_sel == 2'b10) instr = i_rd_data[47:32];
     else                    instr = i_rd_data[63:48];
   end
+  
 
+// test output //
+always @(*) begin
+		$display("m_rdy=%b, i_rdy=%b, i_hit=%b, i_sel=%b, instr=%h,\n m_rd_data=%h, i_rd_data=%h, i_we=%b,\n i_wr_data=%h, m_re=%b, i_addr=%h\n", m_rdy, i_rdy, i_hit, i_sel, instr, m_rd_data, i_rd_data, i_we, i_wr_data, m_re, i_addr);
+		//$display("instr=%h\n",instr);
+	end  
+ 
+  
   
 endmodule
-
