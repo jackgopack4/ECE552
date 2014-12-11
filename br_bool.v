2d15d9e295ba23bf879cb5e2382a80b4e7895c8a
drhartman <drhartman9@gmail.com>
2014-12-10 19:05:04 -0600

    Update br_bool.v

--- br_bool.v
+++ br_bool.v
@@ -1,6 +1,6 @@
 module br_bool(clk,rst_n,clk_z_ID_EX,clk_nv_ID_EX,br_instr_ID_EX,
                jmp_imm_ID_EX,jmp_reg_ID_EX,cc_ID_EX,zr,ov,neg,
-			   zr_EX_DM,flow_change_ID_EX);
+			   zr_EX_DM,flow_change_ID_EX,stall_EX_DM,stall_ID_EX);
 
 //////////////////////////////////////////////////////
 // determines branch or not based on cc, and flags //
@@ -13,6 +13,7 @@ input jmp_imm_ID_EX;		// from ID, tell us this is jump immediate instruction
 input jmp_reg_ID_EX;		// from ID, tell us this is jump register instruction
 input [2:0] cc_ID_EX;		// condition code from instr[11:9]
 input zr,ov,neg;			// flag bits from ALU
+input stall_EX_DM,stall_ID_EX;
 
 output reg flow_change_ID_EX;		// asserted if we should take branch or jumping
 output reg zr_EX_DM;				// goes to ID for ADDZ
@@ -25,7 +26,7 @@ reg neg_EX_DM,ov_EX_DM;
 always @(posedge clk, negedge rst_n)
   if (!rst_n)
     zr_EX_DM  <= 0;
-  else if (clk_z_ID_EX) 
+  else if (clk_z_ID_EX && !stall_EX_DM) 
     zr_EX_DM  <= zr;
 
 	
@@ -38,27 +39,28 @@ always @(posedge clk, negedge rst_n)
       ov_EX_DM  <= 0;
 	  neg_EX_DM <= 0;
 	end
-  else if (clk_nv_ID_EX)
+  else if (clk_nv_ID_EX && !stall_EX_DM)
     begin
       ov_EX_DM  <= ov;
 	  neg_EX_DM <= neg;
 	end
 
-always @(br_instr_ID_EX,cc_ID_EX,zr_EX_DM,ov_EX_DM,neg_EX_DM,jmp_reg_ID_EX,jmp_imm_ID_EX) begin
+//if (!stall_ID_EX) begin
+	always @(br_instr_ID_EX,cc_ID_EX,zr_EX_DM,ov_EX_DM,neg_EX_DM,jmp_reg_ID_EX,jmp_imm_ID_EX) begin
 
-  flow_change_ID_EX = jmp_imm_ID_EX | jmp_reg_ID_EX;	// jumps always change the flow
-  
-  if (br_instr_ID_EX)
-    case (cc_ID_EX)
-	  3'b000 : flow_change_ID_EX = ~zr_EX_DM;
-	  3'b001 : flow_change_ID_EX = zr_EX_DM;
-	  3'b010 : flow_change_ID_EX = ~zr_EX_DM & ~neg_EX_DM;
-	  3'b011 : flow_change_ID_EX = neg_EX_DM;
-	  3'b100 : flow_change_ID_EX = zr_EX_DM | (~zr_EX_DM & ~neg_EX_DM);
-	  3'b101 : flow_change_ID_EX = neg_EX_DM | zr_EX_DM;
-	  3'b110 : flow_change_ID_EX = ov_EX_DM;
-	  3'b111 : flow_change_ID_EX = 1;
-	endcase
-end
-
+	  flow_change_ID_EX = jmp_imm_ID_EX | jmp_reg_ID_EX;	// jumps always change the flow
+	  
+	  if (br_instr_ID_EX)
+		case (cc_ID_EX)
+		  3'b000 : flow_change_ID_EX = ~zr_EX_DM;
+		  3'b001 : flow_change_ID_EX = zr_EX_DM;
+		  3'b010 : flow_change_ID_EX = ~zr_EX_DM & ~neg_EX_DM;
+		  3'b011 : flow_change_ID_EX = neg_EX_DM;
+		  3'b100 : flow_change_ID_EX = zr_EX_DM | (~zr_EX_DM & ~neg_EX_DM);
+		  3'b101 : flow_change_ID_EX = neg_EX_DM | zr_EX_DM;
+		  3'b110 : flow_change_ID_EX = ov_EX_DM;
+		  3'b111 : flow_change_ID_EX = 1;
+		endcase
+	end
+//end
 endmodule
