91bd93a7e8a133e60dd3bc300a3824fb4dcd2040
drhartman <drhartman9@gmail.com>
2014-12-10 19:06:04 -0600

    Update cache_controller.v

--- cache_controller.v
+++ cache_controller.v
@@ -1,35 +1,30 @@
-module cache_controller(clk, rst_n, i_rdy, i_sel, i_wr_data, i_we, m_addr, m_re, m_we, m_wr_data, i_addr, i_hit, i_tag, m_rd_data, m_rdy, re, we, d_addr, wrt_data, d_wr_data, d_dirty_write, d_we, d_re, d_tag, d_hit, d_dirty_read, d_sel, d_rd_data, d_rdy);
+module cache_controller(clk, rst_n, i_rdy, i_sel, i_wr_data, i_we, m_addr, m_re, m_we, m_wr_data, i_addr, i_hit, i_tag, m_rd_data, m_rdy, re, we, d_addr, wrt_data);
   
   input clk, rst_n;
   input [15:0] i_addr;
-  input i_hit, d_hit, d_dirty_read;
-  input [7:0] i_tag, d_tag;
-  input [63:0] m_rd_data, d_rd_data;
+  input i_hit;
+  input [7:0] i_tag;
+  input [63:0] m_rd_data;
   input m_rdy;
   input re, we;
   input [15:0] d_addr, wrt_data;
   
-  output reg i_rdy, i_we, m_we, m_re, d_dirty_write, d_we, d_re, d_rdy;
-  output reg [63:0] i_wr_data, m_wr_data, d_wr_data;
+  output reg i_rdy, i_we, m_we, m_re;
+  output reg [63:0] i_wr_data, m_wr_data;
   output reg [13:0] m_addr;
-  output reg [1:0] i_sel, d_sel;
+  output reg [1:0] i_sel;
   
-  reg [2:0] state, nextState; // allows 4 states should be enough
-  reg [63:0] save_d_rd_data;
-
+  reg [1:0] state, nextState; // allows 4 states should be enough
+  
   // States //
-  localparam IDLE 	       = 3'b000;
-  localparam WRITE_DCACHE  = 3'b001;
-  localparam DCACHE_TO_MEM = 3'b010;
-  localparam MEM_TO_DCACHE = 3'b011;
-  localparam READ_DCACHE   = 3'b100;
-  localparam READ_ICACHE   = 3'b101;
-  localparam MEM_TO_ICACHE = 3'b110;
+  localparam CACHE_READ 	= 2'b00;
+  localparam MEM_READ 	 	= 2'b01;
+  localparam WRITE_BACK		= 2'b10;
 
   
   always @(posedge clk, negedge rst_n) begin
 	if(!rst_n) begin
-      state <= IDLE;
+      state <= CACHE_READ;
     end else begin
       state <= nextState;
     end
@@ -40,79 +35,65 @@ module cache_controller(clk, rst_n, i_rdy, i_sel, i_wr_data, i_we, m_addr, m_re,
         $display("STATE=%b", state);
     // default values
     i_rdy = 1'b0;
-    i_sel = i_addr[1:0];
     i_we  = 1'b0;
-    i_wr_data = m_rd_data;
     m_we  = 1'b0;
     m_re  = 1'b0;
+    i_sel = 2'b00;
+    nextState = CACHE_READ;
     m_addr = i_addr[15:2];
-    m_wr_data = d_rd_data;
-    d_dirty_write = 1'b0;
-    d_we = 1'b0;
-    d_re = 1'b0; // data read from mem is auto put into instr to write to cache
-    d_wr_data = d_rd_data;
-    d_sel = d_addr[1:0];
-    d_rdy = 1'b0;
-
-    nextState = IDLE;
+    i_wr_data = m_rd_data; // data read from mem is auto put into instr to write to cache
+    m_wr_data = 64'b0; //switch to d_rd_data in the future
     
     case (state)
-      IDLE: begin
-        if(we) begin
-          nextState=(d_hit)? WRITE_DCACHE : ((d_dirty_read) ? DCACHE_TO_MEM : MEM_TO_DCACHE);
-        end else if(re) begin
-          nextState=(d_hit)? READ_DCACHE : ((d_dirty_read) ? DCACHE_TO_MEM : MEM_TO_DCACHE);
+    
+      CACHE_READ: begin // receive Valid CPU request, transition to compare tag
+
+        // IF A HIT
+        if (i_hit) begin
+		    $display("icache hit!!");
+		      i_sel = i_addr[1:0];
+		      i_rdy = 1;
+		      nextState = CACHE_READ;
+	 
+		    // IF NOT A HIT
         end else begin
-          nextState = (i_hit) ? READ_ICACHE : MEM_TO_ICACHE;
-        end
+		    // test output
+		    $display("icache miss!!");
+	    	// enable mem read
+			m_re = 1'b1;
+			// enter into mem read for 4 clk cycles
+			nextState = MEM_READ;
+        end     
       end
-      WRITE_DCACHE: begin
-        d_we = 1'b1;
-        d_dirty_write = 1'b1;
-        case (d_addr[1:0])
-          2'b00: d_wr_data[15:0] = wrt_data;
-          2'b01: d_wr_data[31:16] = wrt_data;
-          2'b10: d_wr_data[47:32] = wrt_data;
-          2'b11: d_wr_data[63:48] = wrt_data;
-          default: begin
-            $display("nothing to see here\n");
-          end
-        endcase
-        d_rdy = 1'b1;
-        nextState=(i_hit)? READ_ICACHE: MEM_TO_ICACHE;
+
+      MEM_READ: begin
+		// IF MEM IS NOT READY TO BE READ      
+        if(!m_rdy) begin
+		    // test output
+		    $display("still reading mem");
+        	nextState = MEM_READ;
+        	// mem read must be high until mem is ready
+        	m_re = 1'b1;
+
+        // IF MEMORY IS READY TO BE READ
+        end else begin
+        // test output
+        $display("mem finished! change state to cache_read");
+        	// enable write to cache
+        	i_we = 1'b1;
+      		// set state back to cache read
+        	nextState = CACHE_READ;
+        end 
       end
-      DCACHE_TO_MEM: begin
-        m_we = 1'b1;
-        d_re = 1'b1;
-        m_addr={d_tag,d_addr[7:2]};
-        if(!m_rdy) nextState=DCACHE_TO_MEM;
-        else begin
-          nextState=MEM_TO_DCACHE;
-        end
+       
+      // currently not using this
+      WRITE_BACK: begin
+        
+        // buffer for cache to write
+        nextState = CACHE_READ;
+        
       end
-      MEM_TO_DCACHE: begin
-        m_re = 1'b1;
-        m_addr = d_addr[15:2];
-        d_wr_data = m_rd_data;
-        if(!m_rdy) nextState=MEM_TO_DCACHE;
-        else begin
-          nextState=(we)? WRITE_DCACHE: READ_DCACHE;
-        end
-      end
-      READ_DCACHE: begin
-        d_re = 1'b1;
-        d_rdy = 1'b1;
-        nextState=(i_hit)? READ_ICACHE: MEM_TO_ICACHE;
-      end
-      READ_ICACHE: begin
-        i_rdy = 1'b1;
-        nextState = IDLE;
-      end
-      MEM_TO_ICACHE: begin
-        m_re = 1'b1;
-        if(!m_rdy) nextState=MEM_TO_ICACHE;
-        else nextState=READ_ICACHE;
-      end
+      
       default: begin
       end
     endcase
