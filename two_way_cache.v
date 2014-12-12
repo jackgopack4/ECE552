d5e98761ca750c5ef95d822c85caad4c0ddd5db4
John Peterson <jackgopack4+github@gmail.com>
2014-12-12 02:19:27 -0600

    Add comments to two_way_cache

--- two_way_cache.v
+++ two_way_cache.v
@@ -1,32 +1,32 @@
 module two_way_cache(clk, rst_n, toggle, addr, wr_data, wdirty, we, re, rd_data, tag_out, hit, dirty);
 
 input clk, rst_n;
-input [13:0] addr;
-input [63:0] wr_data;
-input wdirty;
-input we;
-input re;
-input toggle;
+input [13:0] addr; // chopped address for cache block
+input [63:0] wr_data; // data to write into the cache
+input wdirty; // write the dirty bit
+input we; // perform cache write
+input re; // perform cache read
+input toggle; // update LRU bit (added from direct-mapped cache)
 
 output reg hit;
 output dirty;
 output reg [63:0] rd_data;
-output reg [8:0] tag_out;
+output reg [8:0] tag_out; // 9-bit tag (one more bit required with one less index bit)
 
-reg [74:0]mem1[0:31];
-reg [74:0]mem0[0:31];
-reg [31:0] LRU;
-reg [5:0] x;
+reg [74:0]mem1[0:31]; // Upper block in 32-index cache block
+reg [74:0]mem0[0:31]; // lower block in 32-index cache block
+reg [31:0] LRU; // LRU bits for 32-index cache block
+reg [5:0] x; // counter variable for reset
 
-reg [74:0] mem1_line;
-reg [74:0] mem0_line;
+reg [74:0] mem1_line; // register for upper cache block read
+reg [74:0] mem0_line; // register for lower cache block read
 
-wire hit1, hit0;
+wire hit1, hit0; // upper and lower hit signal
 
 reg we_del;
 wire we_filt;
 
-always @(we)
+always @(we) // glitch filter for we
   	we_del <= we;
 assign we_filt = we & we_del;
 
@@ -40,6 +40,7 @@ always @(clk or we_filt or negedge rst_n) begin
 		end
 	end
 	else if(~clk && we_filt) begin
+		// write the cache, based on which was LRU
 		if(LRU[addr[4:0]] == 1'b0) begin
 			//$display("LRU = low, writing");
 			mem0[addr[4:0]] = {1'b1,wdirty,addr[13:5],wr_data};
@@ -50,7 +51,7 @@ always @(clk or we_filt or negedge rst_n) begin
 			mem1[addr[4:0]] = {1'b1,wdirty,addr[13:5],wr_data};
 			if(toggle == 1'b1) LRU[addr[4:0]] = 1'b0;
 		end
-		//if(toggle == 1'b1) LRU[addr[4:0]] = !LRU[addr[4:0]];
+		//if(toggle == 1'b1) LRU[addr[4:0]] = !LRU[addr[4:0]]; wanted to toggle LRU always, but had problems
 	end
 end
 
@@ -62,10 +63,10 @@ always @(clk or re or addr) begin
 	end
 end
 
-// assign outputs
+// assign hit values to match tag while writing or reading and data valid
 assign hit1 = ((mem1_line[72:64]==addr[13:5]) && (re | we)) ? mem1_line[74] : 1'b0;
 assign hit0 = ((mem0_line[72:64]==addr[13:5]) && (re | we)) ? mem0_line[74] : 1'b0;
-
+// assign dirty output if LRU half of desired block is dirty and valid
 assign dirty = (LRU[addr[4:0]]) ? (mem1_line[74]&mem1_line[73]) : (mem0_line[74]&mem0_line[73]);
 always @(*) begin
 	hit = (hit1 | hit0);
